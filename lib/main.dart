859f0737336340e3c235ea25aa653e4d00b0ecca
Phong <ttphong1011@gmail.com>
2024-09-29 13:17:45 +0930

    generate blocs

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza_order/app.dart';
+import 'package:user_repository/user_repository.dart';
 
 import 'simple_bloc_observer.dart';
 
